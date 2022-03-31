7b7db7250d9683cb3a5759214379f47e81b0b13e
Michelle Hardin <93739119+michellehardin@users.noreply.github.com>
2022-03-30 23:23:59 -0500

    Updated Post and User Routes to fix Edit Post

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,28 +2,46 @@ let express = require('express');
 const AppUserModel = require('../models/users.model');
 let router = express.Router();
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-
+const bcrypt = require('bcrypt')
 
 //Register new user
-router.post('/register', (req, res) => {
-    //req.body
-    if(!req.body){
-        return res.status(400).send('Request Body is missing')
-    }
+router.post("/reg", async (req, res) => {
+    
+    //First we will salt and hash the passwords so that they are protected
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    let user = new AppUserModel(req.body)
-    user.save()
-     .then(doc => {
-         if(!doc || doc.length === 0){
-             return res.status(500).send(doc)
-         }
-         res.status(201).send(doc)
-     })
-     .catch(err => {
-         res.status(500).json(err)
-     })
+    //This creates a new User object to be saved in the database -- filling in the required user information
+    const newUser = new AppUserModel({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+    });
+
+    //Because this is an async function, we must await save()
+    const user = await newUser.save();
+    res.status(200).json(user)
 })
+
+//Login
+router.post("/login", async (req, res) => {
+
+    try {
+        const user = await AppUserModel.findOne({ email: req.body.email });
+        !user && res.status(404).json("user not found");
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password")
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+//Logout
+router.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/login");
+});
 
 //Find All Users
 router.get('/findUsers', async (req, res) => {
@@ -49,13 +67,13 @@ router.get('/user', (req, res) => {
 })
 
 //Search for user by name
-router.get('/finduserbyName', (req, res) => {
-    if(!req.query.firstname) {
-        return res.status(400).send("Missing URL Parameter: firstname")
+router.get('/username/:username', (req, res) => {
+    if(!req.params.username) {
+        return res.status(400).send("Missing URL Parameter: username")
     }
 
     AppUserModel.findOne({
-        firstname: req.query.firstname
+        username: req.params.username
     }, req.body)
         .then(doc => {
          res.json(doc)
@@ -65,32 +83,15 @@ router.get('/finduserbyName', (req, res) => {
         })
 })
 
-//Login 
-router.get('/login', (req, res) => {
-    if(!req.query.email || !req.query.password) {
-        return res.status(400).send("Missing URL Parameter")
-    }
-
-    AppUserModel.findOne({
-        email: req.query.email,
-        password: req.query.password
-    }, req.body)
-        .then(doc => {
-         res.json(doc)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-})
 
 // Delete Profile
-router.delete('/delete', async (req, res) => {
-    if(!req.query.email) {
+router.delete('/delete/:email', async (req, res) => {
+    if(!req.params.email) {
         return res.status(400).send("Missing URL Parameter: email")
     }
 
     AppUserModel.findOneAndRemove({
-        email: req.query.email
+        email: req.params.email
     })
         .then(doc => {
          res.json(doc)
@@ -102,13 +103,17 @@ router.delete('/delete', async (req, res) => {
 })
 
 //Update Email
-router.put('/updateEmail', (req, res) => {
-    if(!req.query.email) {
+router.put('/updateEmail/:email', (req, res) => {
+    if(!req.params.email) {
         return res.status(400).send("Missing URL Parameter: email")
     }
 
+    if(req.params.email === req.body.email){
+        return res.status(400).send("No change in email, try again")
+    }
+
     AppUserModel.findOneAndUpdate({
-        email: req.query.email
+        email: req.params.email
     }, req.body, {
         new: true
     })
@@ -121,14 +126,17 @@ router.put('/updateEmail', (req, res) => {
 })
 
 //Update Name
-router.put('/updateName', (req, res) => {
-    if(!req.query.firstname && req.query.lastname) {
+router.put('/updateName/:username', (req, res) => {
+    if(!req.params.username) {
         return res.status(400).send("Missing URL Parameter")
     }
 
+    if(req.params.username === req.body.username){
+        return res.status(400).send("No change in email, try again")
+    }
+
     AppUserModel.findOneAndUpdate({
-        firstname: req.query.firstname,
-        lastname: req.query.lastname
+        username: req.params.username
     }, req.body, {
         new: true
     })
@@ -141,23 +149,27 @@ router.put('/updateName', (req, res) => {
 })
 
 //Update Password
-router.put('/updatePWD', (req, res) => {
-    if(!req.query.password) {
-        return res.status(400).send("Missing URL Parameter: password")
+router.post('/updatePWD/:id/:changeid', async (req, res) => {
+
+    if (req.params.changeid.length < 6){
+        res.status(400).json("Must be at least 6 characters and cannot be empty");
     }
 
-    AppUserModel.findOneAndUpdate({
-        password: req.query.password
-    }, req.body, {
-        new: true
-    })
-        .then(doc => {
-         res.json(doc)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-})
+    const newPWD = req.params.changeid;
+
+    const salt1 = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPWD, salt1);
+
+    await AppUserModel.findByIdAndUpdate(req.params.id, {
+        password: hashedPassword
+    }, {new: true}) 
+    .then(doc => {
+        res.json(doc)
+       })
+       .catch(err => {
+           res.status(500).json(err)
+       })
+}) 
 
 //Add friend
 router.post("/:userId/friends/:friendId", async(req, res) => {
@@ -171,6 +183,27 @@ router.delete("/:userId/remove/:friendId", async(req, res) => {
     let delete1 = await AppUserModel.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true }).populate({ path: "friends", select: "-__v" }).select("-__v");
     let delete2 = await AppUserModel.findOneAndUpdate({ _id: req.params.friendId }, { $pull: { friends: req.params.userId } }, { new: true }).populate({ path: "friends", select: "-__v" }).select("-__v");
     res.json({ message: "Friend Deleted", user: delete1});
+});
+
+//logout 
+router.post("/logout", (req,res) => {
+    res.send("Logging Out")
+})
+
+//Trying to get a user
+router.get("/users", async (req, res) => {
+    const userId = req.query.userId;
+    const username = req.query.username;
+    try {
+      const user = userId
+        ? await AppUserModel.findById(userId)
+        : await AppUserModel.findOne({ username: username });
+      const { password, ...other } = user._doc;
+      res.status(200).json(other);
+      //console.log("Here you are -->", userId)
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports=router
